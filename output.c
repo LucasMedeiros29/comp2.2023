2fa06309f1fe6131028a523e3f630a1222ed2634
Leticia Namie <leticia.namie@hotmail.com>
2023-08-15 21:30:30 -0300

    Primeiros passos

---
+++ output.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main( ) {
+printf("Hello World!");
+printf("Digite o valor C");
+scanf("%d, &a);
+a=2;
+b=3+2-1+5-3+2;
+printf(a);
+printf(b);
+if (2>8) {
+ printf(b);
+printf("Hello Worlds");
+}
+else {
+if (2<8) {
+ printf(a);
+printf("goodbye world");
+}
+else {
+printf("nao sei");
+}
+}
+return 0;
+}
+
